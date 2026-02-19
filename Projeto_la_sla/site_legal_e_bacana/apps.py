cb43acbc302814146a300589f3e2315d77432d1d
MateusGa <mate.g.z.b@gmail.com>
2026-02-19 09:20:41 -0300

    Criação de configuração do projeto (eu acho)

---
+++ Projeto_la_sla/site_legal_e_bacana/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SiteLegalEBacanaConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "site_legal_e_bacana"
---
+++ Projeto_la_sla/site_legal_e_bacana/urls.py
@@ -0,0 +1,7 @@
+#manda isso pro github
+
+from django.urls import path
+
+urlpatterns = [
+    # path("admin/", admin.site.urls),
+]
