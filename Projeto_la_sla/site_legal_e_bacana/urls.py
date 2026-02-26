b6b0549662e84d5d97271d68359a113a8177b659
unknown <mate.g.z.b@gmail.com>
2026-02-26 09:23:53 -0300

    Fiz as coisas

--- Projeto_la_sla/site_legal_e_bacana/urls.py
+++ Projeto_la_sla/site_legal_e_bacana/urls.py
@@ -1,7 +1,9 @@
 #manda isso pro github
 
 from django.urls import path
+from .views import Index
 
 urlpatterns = [
     # path("admin/", admin.site.urls),
+    path("Parte_1", Index.as_view(), name="Le_Start")
 ]
