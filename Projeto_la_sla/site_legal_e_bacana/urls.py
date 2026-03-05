173132d65aab490ce9618b94237103b1054b0040
unknown <mateus.g.z.b@gmail.com>
2026-03-05 09:21:21 -0300

    Mais coisa em HTML e CSS é legal

--- Projeto_la_sla/site_legal_e_bacana/urls.py
+++ Projeto_la_sla/site_legal_e_bacana/urls.py
@@ -1,9 +1,11 @@
 #manda isso pro github
 
 from django.urls import path
-from .views import Index
+from .views import *
 
 urlpatterns = [
     # path("admin/", admin.site.urls),
-    path("Parte_1", Index.as_view(), name="Le_Start")
+    path("Le_Start/", Index.as_view(), name="Le_Start"),
+    path("Sobre_Nos/", Sobre.as_view(), name="sobre"),
+    path("Contatos",Contato.as_view(), name="contatos" )
 ]
--- Projeto_la_sla/site_legal_e_bacana/views.py
+++ Projeto_la_sla/site_legal_e_bacana/views.py
@@ -2,3 +2,9 @@ from django.views.generic import TemplateView
 
 class Index(TemplateView):
     template_name = "site_legal_e_bacana/inicio.html"
+
+class Sobre(TemplateView):
+    template_name = "site_legal_e_bacana/sobre.html"
+
+class Contato(TemplateView):
+    template_name = "site_legal_e_bacana/contatos.html"
