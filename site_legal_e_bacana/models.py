95608906f57fe9d33b7f0000add5cbc4c1be7657
unknown <mate.g.z.b@gmail.com>
2026-03-26 09:12:20 -0300

    Começando a moldar o Diagrama em código (no módulo)

--- site_legal_e_bacana/models.py
+++ site_legal_e_bacana/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
 # Create your models here.
+
+class Subtarefa(models.Model):
+
+    Subtarefa_nome = models.CharField(max_length=50)
+    Subtarefa_concluida = models.BooleanField()
+
+
+class Anexo(models.Model):
+
+    Anexo_nome = models.CharField(max_length=50, verbose_name='Nome do Anexo')
+    Anexo_arquivo = models.FileField(verbose_name='Arquivo')
+    Anexo_tamanho = models.PositiveIntegerField(verbose_name='Tamanho do Arquivo')
+
+class Lembrete(models.Model):
+
+    Lembrete_nome = models.CharField(max_length=50, verbose_name='Nome do lembrete')
+    Lembrete_date = models.DateField(verbose_name='Data do lembrete')
+    Lembrete_desc = models.TextField(max_length=250, verbose_name='Descrição', blank=True, default='Eu sou uma descrição padrão. Ha')
+
+class Ususario(models.Model):
+
+    Usuario_nome = models.CharField(max_length=50, verbose_name='Seu 🫵 nome')
+    Usuario_email = models.EmailField(max_length=254, verbose_name='Seu E-mail')
+    Usuario_senha = models.CharField(max_length=30, verbose_name='Seu nome')
+
+class Evento(models.Model):
+
+    Evento_nome = models.CharField(max_length=50)
+    Evento_tipo = models.CharField(max_length=50)
+    Evento_materia = models.CharField(max_length=50)
+    Evento_entrega = models.DateField()
+    Evento_repetir = models.BooleanField()
